9dc87c817879ed5ae419b852acb93f7135499eaa
Ashton Fei <yunjia.fei@gmail.com>
2021-10-12 19:45:29 +0800

    add LC004 Part B

---
+++ src/projects/LC004/PART_B/Code.js
@@ -0,0 +1,56 @@
+const APP_NAME = "LC004 Form with Multiple Dependent Dropdowns"
+const FILES_FOLDER_NAME = "Images"
+
+/**
+ * Link the html file to another one
+ */
+function link(filename){
+  return HtmlService.createHtmlOutputFromFile(filename).getContent()
+}
+
+/**
+ * Create a file on google drive
+ */
+function createFile(data, name){
+  const id = SpreadsheetApp.getActive().getId()
+  const currentFolder = DriveApp.getFileById(id).getParents().next()
+  const folders = currentFolder.getFoldersByName(FILES_FOLDER_NAME)
+  let folder = null
+  if (folders.hasNext()) {
+    folder = folders.next()
+  }else{
+    folder = currentFolder.createFolder(FILES_FOLDER_NAME)
+  }
+  const [fileType, fileData] = data.slice(5).split(";base64,")
+  const decodedFileData = Utilities.base64Decode(fileData)
+  const blob = Utilities.newBlob(decodedFileData, fileType).setName(name)
+  const file = folder.createFile(blob)
+  return file
+}
+
+/**
+ * Handle get request
+ */
+function doGet() {
+  return HtmlService
+    .createTemplateFromFile("index.html")
+    .evaluate()
+    .setTitle(APP_NAME)
+    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
+    .addMetaTag("viewport", "width=device-width, initial-scale=1.0")
+}
+
+/**
+ * Handle post request
+ */
+function doPost(e){
+  const postData = e.parameter
+  const uuid = Utilities.getUuid()
+  const image = createFile(postData.data, postData.profile)
+  //save to spreadsheet
+  const ws = SpreadsheetApp.getActive().getSheetByName("Responses")
+  ws.appendRow([uuid, postData.college, postData.department, postData.teacher, postData.profile, image.getUrl()])
+  const html = `Thanks for your submission! \n${uuid}`
+  return HtmlService.createHtmlOutput(html).setTitle("Success!")
+}
+
--- src/projects/LC004/README.md
+++ src/projects/LC004/README.md
@@ -4,6 +4,8 @@ A form application built with multiple dependent dropdowns.
 
 ## Links
 
-- [Make a copy](https://docs.google.com/spreadsheets/d/1xHKbFeba-lprIWZ2aWyLmix0sMTSkLE0DXPJdBv39po/copy)
-- [Watch on YouTube](https://youtu.be/J-YEwIDwl_8)
+- [Make a copy - Part A](https://docs.google.com/spreadsheets/d/1xHKbFeba-lprIWZ2aWyLmix0sMTSkLE0DXPJdBv39po/copy)
+- [Watch on YouTube - Part A](https://youtu.be/J-YEwIDwl_8)
+- [Make a copy - Part B](https://docs.google.com/spreadsheets/d/1xipAltAlFYp-vMsEKbrkjTCPUla-sg7q0v0_ywerzs0/copy)
+- [Watch on YouTube - Part B](https://youtu.be/J-YEwIDwl_8)
 - [My YouTube Channel](https://youtube.com/ashtonfei/)
