5eecca600489fa310363e1459c95b35e620374a1
Ashton Fei <yunjia.fei@gmail.com>
2023-12-27 18:58:50 +0800

    add lc023

--- src/projects/LC022/main.js
+++ src/projects/LC022/main.js
@@ -1,79 +1,53 @@
-/**
- * @param {string} value The value to be checked
- * @returns {boolean} true if it's valid formula
- */
-const isFormula_ = (value) => /^=.*[)}"a-z0-9]+$/i.test(value);
+const _createMenu_ =
+  (ui = SpreadsheetApp.getUi()) => (items, caption = null) => {
+    const menu = caption ? ui.createMenu(caption) : ui.createAddonMenu();
+    const createMenuItem = ({ title, items, caption, fn, sep }) => {
+      if (title && items) {
+        return menu.addSubMenu(_createMenu_(ui)(items, title));
+      }
+      if (caption && fn) return menu.addItem(caption, fn);
 
-/**
- * @pram {string[]} headers The headers of the row values in the sheet
- * @pram {objec} item The item object with headers as keys
- * @pram {any[]} values The current values of the row
- * @pram {string[]} formulas The current formulas of the row
- * @returns {any[]} A list of values for the row
- */
-const createRowValues_ = (headers) => (values, formulas) => (item) =>
-  headers.map((header, index) => {
-    const value = header in item ? item[header] : values?.[index];
-    const formula = formulas?.[index];
-    if (!formula) return value;
-    return isFormula_(value) ? value : formula;
-  });
-
-/**
- * @pram {string[]} headers The headers of the row values in the sheet
- * @pram {objec[]} items The item objects with headers as keys
- * @pram {any[][]} values The current values of the range
- * @pram {string[][]} formulas The current formulas of the range
- * @returns {any[][]} A list of values for the range
- */
-const createRangeValues_ = (headers) => (values, formulas) => (items) =>
-  items.map((item, i) =>
-    createRowValues_(headers)(values?.[i], formulas?.[i])(item)
-  );
-
-const test = () => {
-  const headers = ["name", "email", "age", "gender"];
-  const values = [
-    ["Ashton", "", "", ""],
-    ["Ella Zheng", "Link", "", ""],
-  ];
-  const formulas = [
-    ["", "=TODAY()", "", ""],
-    ["", `=HYPERLINK(RC[-1];"Link")`, "", ""],
-  ];
-  const createRangeValues = createRangeValues_(headers)(values, formulas);
-  const ashton = {
-    name: "Ashton Fei",
-    email: "=NOW()",
-    age: 30,
-    gender: "Male",
+      if (sep) return menu.addSeparator();
+    };
+    items.forEach(createMenuItem);
+    return menu;
   };
-  const ella = {
-    name: "Ella Fei",
-    email: "ella@gmail.com",
-    age: 38,
-    gender: "Female",
-  };
-  const users = [ashton, ella];
-  const userValues = createRangeValues(users);
-  console.log(userValues);
+
+const fnToBeDone = () => {
+  SpreadsheetApp.getUi().alert("FN to be done.");
 };
 
-const updateActiveRange = () => {
-  const title = "Update active range";
-  const ss = SpreadsheetApp.getActive();
-  const rangeList = ss.getActiveRangeList();
-  if (rangeList === null) return ss.toast("No selected ranges.", title);
-  const range = rangeList.getRanges()[0];
-  const [headers, ...values] = range.getValues();
-  const formulas = range.getFormulas().slice(0);
-  console.log(headers);
-  console.log(values);
-  console.log(formulas);
-};
+const MENU_ITEMS = [
+  // menu item object with caption and fn
+  { caption: "Func A", fn: "fnToBeDone" },
+  // menu separator object with sep = true
+  { sep: true },
+  // Sub menu object with title and items
+  {
+    title: "Sub Menu",
+    items: [
+      { caption: "Func A", fn: "fnToBeDone" },
+      { sep: true },
+      {
+        title: "Sub Menu",
+        items: [
+          { caption: "Func A ", fn: "fnToBeDone" },
+          { caption: "Func B ", fn: "fnToBeDone" },
+          { caption: "Func C", fn: "fnToBeDone" },
+        ],
+      },
+    ],
+  },
+  { caption: "Func B", fn: "fnToBeDone" },
+];
 
 const onOpen = () => {
-  const menu = SpreadsheetApp.getUi().createMenu("LC022");
-  menu.addItem("Update active range", "updateActiveRange");
+  const ui = SpreadsheetApp.getUi();
+  const buildMenu = _createMenu_(ui);
+  // create a custom menu
+  const menu = buildMenu(MENU_ITEMS, "LC022");
+  // create an addon menu
+  const addonMenu = buildMenu(MENU_ITEMS);
   menu.addToUi();
+  addonMenu.addToUi();
 };
